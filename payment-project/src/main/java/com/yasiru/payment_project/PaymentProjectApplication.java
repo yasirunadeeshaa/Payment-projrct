8608419b8565a1b6a5b096114b36ce5b0622f2ae
A . Yasiru Nadeesha Aththanayaka <yasiru.20232389@iit.ac.lk>
2025-08-20 21:49:07 +0530

    main file

---
+++ payment-project/src/main/java/com/yasiru/payment_project/PaymentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yasiru.payment_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProjectApplication.class, args);
+	}
+
+}
---
+++ payment-project/src/test/java/com/yasiru/payment_project/PaymentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasiru.payment_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
